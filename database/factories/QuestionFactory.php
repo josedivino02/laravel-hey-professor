81a44ecc6d79f9285fed9e0d3f1de83471c58af9
José Divino <josedivinooficial02@gmail.com>
2023-12-02 23:00:09 -0300

    feature-HEY-5: should make sure that only the person who has created the question can publish de question1

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class QuestionFactory extends Factory
@@ -9,8 +10,9 @@ class QuestionFactory extends Factory
     public function definition(): array
     {
         return [
-            'question' => fake()->realText(50),
-            'draft'    => fake()->boolean(),
+            'question'   => fake()->realText(50),
+            'draft'      => fake()->boolean(),
+            'created_by' => User::factory(),
         ];
     }
 }
